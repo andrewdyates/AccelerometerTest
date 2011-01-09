83c1ac9fc475e461c142119dd8492fa7ba8dfae8
Andrew D. Yates <andrew.yates@hhmds.com>
2011-01-09 13:15:00 -0500

    sloppy but working variance computation

--- Resources/app.js
+++ Resources/app.js
@@ -68,6 +68,10 @@ var variance = Titanium.UI.createLabel(
 });
 win.add(variance);
 
+// This should be an object with methods "add sample", "compute variance", 
+// "clear" with "max_sample_size" an init argument
+// 
+// This object should then be imported and unit tested 
 // array of triplets (x,y,z)
 var samples = [];
 // maximum sample size
@@ -81,30 +85,49 @@ function push_sample(x, y, z){
     }
 }
 
-function compute_variance(){
-    var x_sq_sum = 0;
-    var y_sq_sum = 0;
-    var z_sq_sum = 0;
+// this could be optimized to use previous avg and var calcuations
+function compute_variance() 
+{
+    /* DOCSTRING
+     * This should describe the parameters and return values. */
+    var x_sq_sum = 0, x_sum = 0;
+    var y_sq_sum = 0, y_sum = 0;
+    var z_sq_sum = 0, z_sum = 0;
+    var x_avg, y_avg, z_avg;
     var x_var, y_var, z_var;
-    var v;
+    var m2_avg;
+    var n;
 
-    var n = samples.length;
+    n = samples.length;
+
+    // this should be in map-reduce form
     for (var i=0; i<n; i++){
+	x_sum += samples[i][0];
+	y_sum += samples[i][1];
+	z_sum += samples[i][2];
 	x_sq_sum += Math.pow(samples[i][0], 2);
 	y_sq_sum += Math.pow(samples[i][1], 2);
 	z_sq_sum += Math.pow(samples[i][2], 2);
     }
-    // note: x and y averages are 0, z is -1^2 == 1
-    x_var = x_sq_sum / n;
-    y_var = y_sq_sum / n;
-    z_var = (z_sq_sum - n) / n;
+    x_avg = x_sum / n;
+    y_avg = y_sum / n;
+    z_avg = z_sum / n;
 
-    v = (x_var + y_var + z_var) / 3;
+    // these should be standard deviations
+    x_var = (x_sq_sum - (n * Math.pow(x_avg, 2))) / n;
+    y_var = (y_sq_sum - (n * Math.pow(y_avg, 2))) / n;
+    z_var = (z_sq_sum - (n * Math.pow(z_avg, 2))) / n;
+
+    // this should be an average of standard deviations
+    // this could also be calculated from s_sqaured from each dimension
+    m2_avg = (x_var + y_var + z_var) / 3;
+
     // debug negative variance
-    if (v < 0) {
+    if (m2_avg < 0) {
 	alert("Warning! v=" + v + " x_var=" + x_var + " y_var=" + y_var + "z_var=" + z_var);
     }
-    return v;
+
+    return m2_avg;
 }
 
 
