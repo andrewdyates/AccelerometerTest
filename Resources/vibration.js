675e86a4fb2345181c9b77fce4a175af6782550e
Andrew D. Yates <andrew.yates@hhmds.com>
2011-01-18 08:30:34 -0500

    Cleaned variance computation, changed to standard deviation. No testing yet.

--- Resources/vibration.js
+++ Resources/vibration.js
@@ -11,78 +11,54 @@
 
 var DFT_SAMPLE_SIZE = 20;
 
-function Sampler(size) {
-    /* Fixed-size circular numeric array for collecting data samples.
+
+function push_rotate(v, x, size) {
+    /* Add element to fixed-size array. 
      * 
      * Arguments:
-     *   size: int > 0 of number of samples to store. Default: DFT_SAMPLE_SIZE
-     * 
-     * Properties:
-     *   last_pushed: numeric of most recently added sample, or null
-     *   last_shifted: numeric of most recently removed sample, or null
-     *   samples: array[numeric] of samples of length <= `size`
-     *   get_size: return int > 0 of this Sampler's maximum size
-     *   is_populated return bool if Sampler is populated with > `size` samples
-     */
-    if(!size) { size = DFT_SAMPLE_SIZE; } 
-
-    this.last_pushed = null;
-    this.last_shifted = null;
-    this.samples = [];
-
-    this.get_size = function() { return size; };
-    this.is_populated = function() {
-	return this.last_pushed && this.last_shifted; 
-    };
-
-    this.push = function(x) {
-	/* Add numeric sample to Sampler.
-	 * 
-	 * Arguments:
-	 *   x: numeric value to add to Sampler
-	 * Returns:
-	 *   numeric value shifted off Sampler or null.
-	 */
-	this.last_pushed = x;
-	samples.push(x);
-	if (samples.length > size) {
-	    this.last_shifted = samples.shift();
-	}
-	return this.last_shifted;
-    };
-}
-
-function variance(v) {
-    var sum, sum_of_sqs, avg;
-    sum = v.reduce(function(a, b){return a + b;}, 0);
-    sum_of_sqs = v.reduce(function(a, b){return a + b*b;}, 0);
-    avg = sum / v.size;
-    return sum_of_sqs / v.size - avg*avg;;
-}
-
-function sum(v, prev_sum, less_x, plus_x) {
-    /* Sum a vector. Compute using previous values if available. 
-     * 
-     * Arguments:
-     *   v: Array of num; any NaN, null, or undefined members assumed == 0
-     *   prev_sum: [optional] num of previously computed sum
-     *   less_x: [optional] num of member to subtract from prev_sum
-     *   plus_x: [optional] num of member to add to prev_sum
-     * 
+     *   v: Array
+     *   x: item to add to Array
+     *   size: int > 0, >= |v| of max array size
+     * Modifies:
+     *   v: adds `x` as first item, may remove last item
      * Returns:
-     *   num of sum of v
+     *   item removed from `v` or null
      * */
-    if (prev_sum && less_x && plus_x) {
-	return prev_sum - less_x + plus_x;
+    var last;
+    v.push(x);
+    if (v.length > size) {
+	last = v.shift();
     } else {
-	return v.reduce(function(a, b){return a + b;}, 0);
+	last = null;
     }
+    return last;
 }
 
-function square(x){return x * x;}
+function std_deviation(v, last_item, last_sum, last_sum_sqs) {
+    /* Return the standard deviation of an array of numbers.
+     * 
+     * Arguments:
+     *   v: Array of num
+     *   last: [optional] num of last item removed from v
+     *   last_sum: [optional] num of the sum of v(n-1)
+     *   last_sum_sqs: [optional] num of the sum of squares of v(n-1)
+     * Returns:
+     *   num of sample variance
+     */
+    var sum, sum_of_sqs, avg, sigma_sq, sigma;
 
-function squares(v){return v.map(square);}
+    // compute or update sums
+    if(last_item && last_sum && last_sum_sqs) {
+	var new_item = v[v.length - 1];
+	sum = last_sum - last_item + new_item;
+	sum_sqs = last_sum_sqs - last_item^2 + new_item^2;
+    } else {
+	sum = v.reduce(function(a, b){return a + b;});
+	sum_sqs = v.reduce(function(a, b){return a + b^2;});
+    }
 
-function variance(v, last_sum, last_sum_sqs) {
-    
+    avg = sum / v.length;
+    sigma_sq = sum_sqs / v.length - avg^2;
+    sigma = Math.sqrt(sigma_sq);
+    return sigma;
 }
