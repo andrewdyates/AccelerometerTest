09d9798eae70fd984bb424094aca04f967212129
Andrew D. Yates <andrew.yates@hhmds.com>
2011-01-18 19:54:36 -0500

    vibrations update

--- Resources/vibration.js
+++ Resources/vibration.js
@@ -32,31 +32,39 @@ function push_rotate(v, x, size) {
     return last;
 }
 
-function std_deviation(v, last_item, last_sum, last_sum_sqs) {
+function std_deviation(v, cache) {
     /* Return the standard deviation of an array of numbers.
      * 
      * Arguments:
      *   v: Array of num
-     *   last: [optional] num of last item removed from v
-     *   last_sum: [optional] num of the sum of v(n-1)
-     *   last_sum_sqs: [optional] num of the sum of squares of v(n-1)
+     *   cache: [optional] dict of
+     *     last: num of last item removed from v
+     *     last_sum: num of the sum of last v
+     *     last_sum_sqs: num of the sum of squares of last v
+     * Modifies:
+     *   cache: updates corresponding values
      * Returns:
      *   num of sample variance
      */
     var sum, sum_of_sqs, avg, sigma_sq, sigma;
+    if(cache == undefined) cache = {};
 
     // compute or update sums
-    if(last_item && last_sum && last_sum_sqs) {
+    if(cache.item && cache.sum && cache.sum_sqs) {
 	var new_item = v[v.length - 1];
-	sum = last_sum - last_item + new_item;
-	sum_sqs = last_sum_sqs - Math.pow(last_item, 2) + Math.pow(new_item, 2);
+	sum = cache.sum - cache.item + new_item;
+	sum_sqs = cache.sum_sqs - cache.item*cache.item + new_item*new_item;
     } else {
 	sum = v.reduce(function(a, b){return a + b;}, 0);
 	sum_sqs = v.reduce(function(a, b){return a + b*b;}, 0);
     }
+    // update computed cache values
+    cache.sum = sum;
+    cache.sum_sqs = sum_sqs;
 
     avg = sum / v.length;
-    sigma_sq = sum_sqs / v.length - Math.pow(avg, 2);
+    sigma_sq = sum_sqs / v.length - avg*avg;
     sigma = Math.sqrt(sigma_sq);
     return sigma;
 }
+
