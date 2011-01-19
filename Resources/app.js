046550375bda6c3d7c4934019a8a4f2eb9178144
Andrew D. Yates <andrew.yates@hhmds.com>
2011-01-18 23:09:01 -0500

    updated icon images, some changes to vibrations to be debugged

--- Resources/app.js
+++ Resources/app.js
@@ -1,3 +1,5 @@
+Ti.include("vibration.js");
+
 // Window
 Titanium.UI.setBackgroundColor('#000');
 var win = Titanium.UI.createWindow(
@@ -58,77 +60,15 @@ win.add(ts);
 
 var variance = Titanium.UI.createLabel(
 {
-    text:'variance:',
+    text:'deviation:',
     top:90,
     left:10,
-    font:{fontSize:14},
+    font:{fontSize:18},
     color:'#555',
     width:300,
     height:'auto'
 });
 win.add(variance);
-
-// This should be an object with methods "add sample", "compute variance", 
-// "clear" with "max_sample_size" an init argument
-// 
-// This object should then be imported and unit tested 
-// array of triplets (x,y,z)
-var samples = [];
-// maximum sample size
-var MAX_SAMPLE_SIZE = 20;
-// units of g (m/s)
-
-function push_sample(x, y, z){
-    samples.push([x,y,z]);
-    if (samples.length > MAX_SAMPLE_SIZE) {
-	samples.shift();
-    }
-}
-
-// this could be optimized to use previous avg and var calcuations
-function compute_variance() 
-{
-    /* DOCSTRING
-     * This should describe the parameters and return values. */
-    var x_sq_sum = 0, x_sum = 0;
-    var y_sq_sum = 0, y_sum = 0;
-    var z_sq_sum = 0, z_sum = 0;
-    var x_avg, y_avg, z_avg;
-    var x_var, y_var, z_var;
-    var m2_avg;
-    var n;
-
-    n = samples.length;
-
-    // this should be in map-reduce form
-    for (var i=0; i<n; i++){
-	x_sum += samples[i][0];
-	y_sum += samples[i][1];
-	z_sum += samples[i][2];
-	x_sq_sum += Math.pow(samples[i][0], 2);
-	y_sq_sum += Math.pow(samples[i][1], 2);
-	z_sq_sum += Math.pow(samples[i][2], 2);
-    }
-    x_avg = x_sum / n;
-    y_avg = y_sum / n;
-    z_avg = z_sum / n;
-
-    // these should be standard deviations
-    x_var = (x_sq_sum - (n * Math.pow(x_avg, 2))) / n;
-    y_var = (y_sq_sum - (n * Math.pow(y_avg, 2))) / n;
-    z_var = (z_sq_sum - (n * Math.pow(z_avg, 2))) / n;
-
-    // this should be an average of standard deviations
-    // this could also be calculated from s_sqaured from each dimension
-    m2_avg = (x_var + y_var + z_var) / 3;
-
-    // debug negative variance
-    if (m2_avg < 0) {
-	alert("Warning! v=" + v + " x_var=" + x_var + " y_var=" + y_var + "z_var=" + z_var);
-    }
-
-    return m2_avg;
-}
 
 
 // Accelerometer Event listener
@@ -139,9 +79,9 @@ Ti.Accelerometer.addEventListener('update',function(e)
     y.text = 'y:' + e.y;
     z.text = 'z:' + e.z;
 
-    push_sample(e.x, e.y, e.z);
-    v = compute_variance();
-    variance.text = 'variance:' + v;
+    Sampler.push(e.x, e.y, e.z);
+    v = Sampler.avg_deviation();
+    variance.text = 'deviation:' + v;
 });
 
 // note: Accelerometer does not work in simulator
--- Resources/vibration.js
+++ Resources/vibration.js
@@ -11,6 +11,80 @@
 
 var DFT_SAMPLE_SIZE = 20;
 
+// there should be a "Sampler object" here
+// init to a size, push triplets, keeps own cache
+// returns useful values when queried
+
+Sampler = {
+    size: DFT_SAMPLE_SIZE,
+    // sample vector
+    items: [],
+    // cache
+    cache: {
+	item: null,
+	sums: null,
+	sum_sqs: null
+    },
+//    averages
+//    deviations
+    
+    push: function(x, y, z) {
+	this.v.push([x,y,z]);
+	if (this.v.length > this.size) {
+	    this.cache.item = this.v.shift();
+	}
+    },
+
+    avg_deviation: function() {
+	var sums = [0, 0, 0];
+	var sq_sums = [0, 0, 0];
+	var devs = [null, null, null];
+	var avgs = [null, null, null];
+	var dev_avg;
+	var n;
+	var i, k;
+
+	n = this.items.length;
+	
+	// compute sums if the cache is incomplete
+	if (!(this.cache.item && this.cache.sums && this.cache.sum_sqs)) {
+	    for (i=0; i<n; i++) {
+		for (k=0; k<=2; k++) {
+		    sums[k] += this.items[i][k];
+		    sq_sums[k] += Math.pow(this.items[i][k], 2);
+		}
+	    }
+	// update sums from cache values otherwise
+	} else {
+	    for (k=0; k<=2; k++) {
+		sums[k] = this.cache.sums[k] - 
+		    this.cache.item[k] + this.items[n-1][k];
+		sq_sums[k] = this.cache.sq_sums[k] - 
+		    Math.pow(this.cache.item[k], 2) + 
+		    Math.pow(this.items[n-1][k]);
+	    }
+	}
+
+	// update cache
+	this.cache.sums = sums.slice();
+	this.cache.sq_sums = sq_sums.slice();
+	this.cache.item = this.items[n-1].slice();
+
+	// compute deviations
+	for (k=0; k<=2; k++) {
+	    avg[k] = sums[k] / n;
+	    dev[k] = Math.sqrt(sq_sums[k] / n - Math.pow(avg[k], 2));
+	}
+
+	// return average deviation over three dimensions
+	dev_avg = (dev[0] + dev[1] + dev[2]) / 3;
+	return dev_avg;
+    }
+};
+
+function push_xyz(v, x, y, z) {
+    last_set = v.push_rotate([x,y,z]);
+}
 
 function push_rotate(v, x, size) {
     /* Add element to fixed-size modular array. 
@@ -25,6 +99,7 @@ function push_rotate(v, x, size) {
      *   item shifted from v[0] or null
      * */
     var last = null;
+    size = size || DFT_SAMPLE_SIZE;
     v.push(x);
     if (v.length > size) {
 	last = v.shift();
@@ -47,7 +122,7 @@ function std_deviation(v, cache) {
      *   num of sample variance
      */
     var sum, sum_of_sqs, avg, sigma_sq, sigma;
-    if(cache == undefined) cache = {};
+    cache = cache || {};
 
     // compute or update sums
     if(cache.item && cache.sum && cache.sum_sqs) {
