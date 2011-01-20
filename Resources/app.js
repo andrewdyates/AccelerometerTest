4ee34922dc0062a9779e2e97b32b8974eee2720b
Andrew D. Yates <andrew.yates@hhmds.com>
2011-01-20 17:57:14 -0500

    working event flags

--- Resources/app.js
+++ Resources/app.js
@@ -1,5 +1,6 @@
 Ti.include("vibration.js");
 
+
 // Window
 Titanium.UI.setBackgroundColor('#000');
 var win = Titanium.UI.createWindow(
@@ -61,7 +62,7 @@ win.add(ts);
 var variance = Titanium.UI.createLabel(
 {
     text:'deviation:',
-    top:90,
+    top:85,
     left:10,
     font:{fontSize:18},
     color:'#555',
@@ -69,6 +70,36 @@ var variance = Titanium.UI.createLabel(
     height:'auto'
 });
 win.add(variance);
+
+var is_vibrating_label = Titanium.UI.createLabel(
+{
+    text:'is_vibrating: ',
+    top:105,
+    left:10,
+    font:{fontSize:18},
+    color:'#555',
+    width:300,
+    height:'auto'
+});
+win.add(is_vibrating_label);
+
+// if avg variance is <, assume not vibrating, 
+// else assume vibration
+var VIBRATION_THRESHOLD = 0.00009;
+var is_vibrating = false;
+
+
+Ti.App.addEventListener('stop_vibration', function(e) 
+{
+    is_vibrating = false;
+    is_vibrating_label.text = "is_vibrating: " + is_vibrating;
+});
+
+Ti.App.addEventListener('start_vibration', function(e) 
+{
+    is_vibrating = true;
+    is_vibrating_label.text = "is_vibrating: " + is_vibrating;
+});
 
 
 // Accelerometer Event listener
@@ -84,6 +115,14 @@ Ti.Accelerometer.addEventListener('update',function(e)
     Sampler.push(e.x, e.y, e.z);
     v = Sampler.variance();
     variance.text = 'v:' + v;
+
+    // vibration handler test
+    if (!is_vibrating && v > VIBRATION_THRESHOLD) {
+	Ti.App.fireEvent('start_vibration', {v:v});
+    } else if (is_vibrating && v <= VIBRATION_THRESHOLD) {
+	Ti.App.fireEvent('stop_vibration', {v:v});
+    }
+    
 });
 
 // note: Accelerometer does not work in simulator
