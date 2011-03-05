100735d1a2d5ac6c3bb29a38bd6c45b413b67b59
Andrew D. Yates <andrew.yates@hhmds.com>
2011-03-05 16:19:18 -0500

    Units updated

--- Resources/app.js
+++ Resources/app.js
@@ -1,5 +1,5 @@
 // note: Accelerometer does not work in simulator
-Ti.include("vibration.js");
+Ti.include("vibration_js/vibration.js");
 
 
 // Window
@@ -14,91 +14,25 @@ var win = Titanium.UI.createWindow(
 // Labels
 var version = Titanium.UI.createLabel(
 {
-    text:'version: 0.894',
-    top:240,
-    font:{fontSize:12},
+    text:'Vibration Test version: 0.895',
+    top:10,
+    font:{fontSize:14},
     color:'#555',
     width:300,
     height:'auto'
 });
 win.add(version);
-var x = Titanium.UI.createLabel(
-{
-    text:'x:',
-    top:10,
-    left:10,
-    font:{fontSize:14},
-    color:'#555',
-    width:300,
-    height:'auto'
-});
-win.add(x);
-var y = Titanium.UI.createLabel(
-{
-    text:'y:',
-    top:30,
-    left:10,
-    font:{fontSize:14},
-    color:'#555',
-    width:300,
-    height:'auto'
-});
-win.add(y);
-var z = Titanium.UI.createLabel(
-{
-    text:'z:',
-    top:50,
-    left:10,
-    font:{fontSize:14},
-    color:'#555',
-    width:300,
-    height:'auto'
-});
-win.add(z);
-var ts = Titanium.UI.createLabel(
-{
-    text:'timestamp:',
-    top:70,
-    left:10,
-    font:{fontSize:14},
-    color:'#555',
-    width:300,
-    height:'auto'
-});
-win.add(ts);
-var variance = Titanium.UI.createLabel(
-{
-    text:'v:',
-    top:85,
-    left:10,
-    font:{fontSize:14},
-    color:'#555',
-    width:300,
-    height:'auto'
-});
-win.add(variance);
-var mean = Titanium.UI.createLabel(
-{
-    text:'u:',
-    top:115,
-    left:10,
-    font:{fontSize:14},
-    color:'#555',
-    width:300,
-    height:'auto'
-});
-win.add(mean);
 var units = Titanium.UI.createLabel(
 {
-    text:'Units in mm/s^2',
-    top:130,
+    text:'Units in mG',
+    top:60,
     left:10,
     font:{fontSize:14},
     color:'#555',
     width:300,
     height:'auto'
 });
-win.add(mean);
+win.add(units);
 var scale_div = Titanium.UI.createLabel(
 {
     text:'20',
@@ -116,18 +50,11 @@ win.add(scale_div);
 Ti.App.addEventListener('vibration_update', function(e) 
 {
     var v, display;
-    
     v = e.vibration;
-    variance.text = 'v:' + v + " mm/s^2";
-
     display = v.toFixed(1);
     scale_div.text = display;
-
 });
 
-Ti.App.fireEvent('vibration_update', {vibration: 5});
-
 Vibration.start();
-
 
 win.open();
